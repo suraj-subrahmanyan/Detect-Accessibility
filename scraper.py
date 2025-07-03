c80688fa333120eb12988d84108cc07830a4ecfb
Suraj Subrahmanyan <ssubrahmanyan11@gmail.com>
2025-07-03 15:23:40 -0400

    Basic web scraper complete applied on books.toscrape.com

--- scraper.py
+++ scraper.py
@@ -1,3 +1,21 @@
-import requests
+def save(html, path):
+    with open(path, 'wb') as f:
+        f.write(html)
 
-getWebsite = requests
+def open(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
+# Import Libraries
+import requests
+from bs4 import BeautifulSoup
+
+# Get page content
+
+page_url = "https://books.toscrape.com/"
+getWebsite = requests.get(page_url)
+
+soup = BeautifulSoup(getWebsite.content, 'html.parser')
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
